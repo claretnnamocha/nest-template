43305a9bff8217caec6aad0bd29af94212d0b253
PhreeTech027 <devclareo@gmail.com>
2023-12-24 10:44:40 +0100

    security

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { logger } from './common';
 import { ErrorFilter } from './common/exception-filters';
 import { ResponseInterceptor } from './common/response-interceptors';
-import { logger } from './common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = 8080;
 
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new ErrorFilter());
+
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100,
+    }),
+  );
 
   await app.listen(port);
 
