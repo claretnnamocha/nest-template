9c5b7288a1415141bcd352e60f0f19edaff0fcbc
Claret Nnamocha <devclareo@gmail.com>
2025-04-20 02:07:27 +0100

    feat: add localized messages for profile data retrieval in multiple languages

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CatchServiceErrors } from 'src/common/base.service';
+import { translate } from 'src/common/i18n';
 import { BaseService } from '../common';
 import { User } from '../common/database/models';
 import { ServiceResponse } from '../common/interfaces';
-import { CatchServiceErrors } from 'src/common/base.service';
 
 @CatchServiceErrors()
 @Injectable()
 export class ProfileService extends BaseService {
   async getProfile(email: string): Promise<ServiceResponse> {
     const user = await User.findOne({ where: { email } });
-    return { success: true, data: user, message: 'Profile details retrieved' };
+    return {
+      success: true,
+      data: user,
+      message: translate('MESSAGES.PROFILE_DATA_RETRIEVED'),
+    };
   }
 }
